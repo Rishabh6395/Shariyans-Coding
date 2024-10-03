7eb3166dbdd3bfbc3bc52ce617870ca9f0851d82
Rishabh <rishabhbhatt956@gmail.com>
2024-10-03 15:04:27 +0530

    first-commit

---
+++ 01-Youtube-Backend/script.js
@@ -0,0 +1,35 @@
+// Fundamental of JavaScript
+// Arrays and Objects
+// Functions return
+// async js coding
+// foreach map filter find indexOf
+
+// var arr = [1, 2, 3, 4, 5];
+// var ans = arr.map(function(val){
+//     return val*3;
+// })
+// console.log(ans)
+
+
+// var arr = [1,2,3,4];
+
+// var ans = arr.filter(function(val){
+//     if(val>3) return true
+//     else return false
+// })
+// console.log(ans);
+
+
+// var arr = [1,2,3,4];
+
+// var ans = arr.find(function(val){
+//     if(val ===2) return val    
+// })
+// console.log(ans);
+
+
+var obj = {
+    name: "rishabh"
+}
+let a = obj.name
+console.log(a);
