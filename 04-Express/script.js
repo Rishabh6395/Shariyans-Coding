e362c97b966881ca3e652705159a7c5226cf6e0a
Rishabh <rishabhbhatt956@gmail.com>
2024-10-04 18:25:22 +0530

    Intro to Express

---
+++ 04-Express/script.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use(function(req, res, next){
+    console.log("Middle ware chla")
+    next()
+})
+app.use(function(req, res, next){
+    console.log("Middle ware chla ek bar or")
+    next()
+})
+
+app.get('/', function(req, res){
+    res.send("Hello World");    
+})
+app.get('/profile', function(req, res, next){
+    return next(new Error("Something went wrong"))   
+})
+
+app.use(function(err, req, res, next){
+    console.log(err.stack)
+    res.status(500).send("Something Broke :(")
+    
+})
+
+app.listen(3000)
