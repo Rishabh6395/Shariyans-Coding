ba7d8b49dd205ebc7bc5d47b443f86d0db7110a5
Rishabh <rishabhbhatt956@gmail.com>
2024-10-18 12:48:03 +0530

    Adding a edit feature

--- 09-CRUD-operation/app.js
+++ 09-CRUD-operation/app.js
@@ -15,6 +15,41 @@ app.get('/read', async (req,res) => {
     let users = await userModel.find()
     res.render("read", {users})
 })
+// Route for rendering the edit form
+app.get('/edit/:id', async (req, res) => {
+    try {
+        let user = await userModel.findOne({ _id: req.params.id }); // Use req.params.id here
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.render("edit", { user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Route for handling form submission and updating the user
+app.post('/update/:id', async (req, res) => { // Change to req.params.id
+    try {
+        let { name, email, image } = req.body;
+        let user = await userModel.findOneAndUpdate(
+            { _id: req.params.id }, // Use req.params.id here
+            { name, email, image },
+            { new: true }
+        );
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        res.redirect("/read");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 app.get('/create', (req,res) => {
     res.render("nice")
 })
