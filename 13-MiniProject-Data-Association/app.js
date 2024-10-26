dcd3cb5400c4494286ce53cc8fce9573b90d3993
Rishabh <rishabhbhatt956@gmail.com>
2024-10-26 06:14:32 +0530

    Adding Like and edit post features and some additional changes

--- 13-MiniProject-Data-Association/app.js
+++ 13-MiniProject-Data-Association/app.js
@@ -22,6 +22,28 @@ app.get('/profile', isLoggedIn, async (req, res)=>{
     let user = await userModel.findOne({email: req.user.email}).populate('posts')
     res.render("profile", {user})
 })
+app.get('/like/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOne({_id: req.user.id}).populate('user')
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid)
+    } 
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+    }
+    
+    await post.save()
+    res.redirect("/profile")
+})
+app.get('/edit/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOne({_id: req.user.id}).populate('user')
+    res.render("edit", {post})
+})
+
+app.post('/update/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOneAndUpdate({_id: req.user.id}, {content: req.body.content})
+    res.redirect("/profile")
+})
+
 app.get('/post', isLoggedIn, async (req, res)=>{
     let user = await userModel.findOne({email: req.user.email})
     let {content} = req.body
