724ecf354def5374a6d570ee5650b3258a2a224e
Rishabh <rishabhbhatt956@gmail.com>
2024-10-27 05:37:20 +0530

    Adding Multer

--- 13-MiniProject-Data-Association/app.js
+++ 13-MiniProject-Data-Association/app.js
@@ -5,14 +5,37 @@ const postModel = require('./model/post')
 const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cypto = require('crypto')
+const path = require('path')
+const multer = require('multer')
 
 app.set("view engine", 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './public/images/uploads')
+    },
+    filename: function(req, file, cb){
+        crypto.randomBytes(12, function(err, bytes){
+            const fn = bytes.toString('hex') + path.extname(file.originalname)
+            cb(null, fn)
+        })
+    }
+})
+
+const upload = multer({ storage: storage })
+
 app.get('/', (req, res)=>{
     res.render("index")
+})
+app.get('/test', (req, res)=>{
+    res.render("test")
+})
+app.post('/upload', upload.single('image'), (req, res)=>{
+    console.log(req.file)
 })
 app.get('/login', (req, res)=>{
     res.render("login")
