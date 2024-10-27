5ee0871724fdc83c7f5b5c45007bf63afa855202
Rishabh <rishabhbhatt956@gmail.com>
2024-10-28 05:05:18 +0530

    some additional changes

--- 13-MiniProject-Data-Association/app.js
+++ 13-MiniProject-Data-Association/app.js
@@ -7,36 +7,19 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cypto = require('crypto')
 const path = require('path')
-const multer = require('multer')
+const multerconfig = require('./config/multerconfig')
 
 app.set("view engine", 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './public/images/uploads')
-    },
-    filename: function(req, file, cb){
-        crypto.randomBytes(12, function(err, bytes){
-            const fn = bytes.toString('hex') + path.extname(file.originalname)
-            cb(null, fn)
-        })
-    }
-})
 
-const upload = multer({ storage: storage })
 
 app.get('/', (req, res)=>{
     res.render("index")
 })
-app.get('/test', (req, res)=>{
-    res.render("test")
-})
-app.post('/upload', upload.single('image'), (req, res)=>{
-    console.log(req.file)
-})
+
 app.get('/login', (req, res)=>{
     res.render("login")
 })
---
+++ 13-MiniProject-Data-Association/config/multerconfig.js
@@ -0,0 +1,20 @@
+const multer = require('multer')
+const path = require('path')
+const crypto = require('crypto')
+
+// disk storage
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './public/images/uploads')
+    },
+    filename: function(req, file, cb){
+        crypto.randomBytes(12,function(err,name){
+            const fn = name.toString('hex')+path.extname(file.originalname)
+            cb(null, fn)
+        })
+    }
+})
+
+const uploads = multer({storage: storage})
+
+module.exports = uploads
